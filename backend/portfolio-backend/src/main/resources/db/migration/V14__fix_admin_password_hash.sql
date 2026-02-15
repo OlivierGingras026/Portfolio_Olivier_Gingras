493acbb230d94388b947d373185990db8c1d28d2
OlivierGingras026 <ogingras013@gmail.com>
2026-02-15 18:44:46 -0500

    remove file test

--- backend/portfolio-backend/src/main/resources/db/migration/V14__fix_admin_password_hash.sql
+++
@@ -1,8 +0,0 @@
--- V14__fix_admin_password_hash.sql
--- Update the admin password hash to a version that works properly
--- This hash is BCrypt encoded "password" - test account
-
-UPDATE admins
-SET password = '$2a$10$dXJ3SW6G7P50eS3lUZtVO.cF0FGxGX7w5PoY2J6xYGcV7iUWlDvxu'
-WHERE email = 'admin@portfolio.com';
-
